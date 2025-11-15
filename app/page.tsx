c2b2ffc3768c73dfdc8bb765e1c797b49a917c71
inacode-cmd <gurungk003@gmail.com>
2025-11-15 11:04:49 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 bg-red-500">
       <ChatInterface />
+      {/* sdsds */}
     </main>
   );
 }
